089d5e88d97909c00596b6fe4930ed05d44d9784
yxgjls <zhaoheng109@gmail.com>
2024-11-20 11:18:34 +0800

    Revert "change categories"
    This reverts commit a8ad8bba86286c71497cbc33f2e6f13df91ccd81.

--- public/blogger-script.js
+++ public/blogger-script.js
@@ -2,6 +2,5 @@ fetch('https://blogger-project-vert.vercel.app/api')
     .then(response => response.json())
     .then(data => {
         console.log(data); // 在前端处理数据
-        document.getElementById('test').textContent = data;
     })
     .catch(error => console.error('Error:', error));
